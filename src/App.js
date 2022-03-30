545fe57919143697965a04e2b3b9cb467f6c017b
MauricioRaulFerreyra <mauriciotauro82@hotmail.com>
2022-03-30 14:29:57 -0300

    folder created

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
-const routes = require('./routes/index.js')
+//const routes = require('./routes/index.js')
 
 require('./db.js')
 
@@ -26,7 +26,7 @@ server.use((req, res, next) => {
   next()
 })
 
-server.use('/', routes)
+server.use('/api', require('./routes'))
 
 // Error catching endware.
 server.use((err, req, res, next) => {

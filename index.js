ab84a5dbda46b55bfa27d07c035d6c4e253f245a
Lean65 <92610599+Lean65@users.noreply.github.com>
2022-03-30 16:01:23 -0300

    Revert "Revert "Dev""

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const server = require('./src/app')
 const { conn } = require('./src/db')
+require('dotenv').config()
 
-conn.sync({force:true}).then(()=>{
-    server.listen(3001, ()=>{console.log('listening')})
+
+const logs = require('./src/logs')
+const loggerConsola = logs.getLogger('consola')
+const loggerError = logs.getLogger('error')
+
+const PORT = process.env.PORT || 3001
+
+conn.sync({ force: true }).then(() => {
+  server.listen(PORT, () => {
+    loggerConsola.info(`Server is run on port ${PORT}`, server.settings.env)
+  })
+  server.on('error', error => loggerError.error(`Error en servidor ${error}`))
 })

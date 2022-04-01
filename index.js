9571e69fa493519dc0706590091aba84151d224c
Lean65 <leandromelerio@gmail.com>
2022-04-01 19:28:05 -0300

    updated index.js

--- index.js
+++ index.js
@@ -8,14 +8,11 @@ const loggerConsola = logs.getLogger('consola')
 const loggerError = logs.getLogger('error')
 
 const PORT = process.env.PORT || 3001
+const HOST = '0.0.0.0'
 
 conn.sync({ force: true }).then(() => {
-  server.listen(PORT, () => {
+  server.listen(PORT, HOST, () => {
     loggerConsola.info(`Server is run on port ${PORT}`, server.settings.env)
   })
   server.on('error', error => loggerError.error(`Error en servidor ${error}`))
 })
-
-// conn.sync({force:true}).then(()=>{
-//     server.listen(3001, ()=>{console.log('listening')})
-// })

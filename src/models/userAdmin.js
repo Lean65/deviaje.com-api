623e2143c4591bd3c8517407a3435cce5be33b32
Lean65 <leandromelerio@gmail.com>
2022-04-03 19:32:28 -0300

    modified models names

--- src/models/userAdmin.js
+++ src/models/userAdmin.js
@@ -1,7 +1,7 @@
 const { DataTypes } = require('sequelize')
 
 module.exports = sequelize => {
-  sequelize.define('userAdmin', {
+  sequelize.define('admin', {
     mail: {
       type: DataTypes.STRING,
       allowNull: false
--- src/models/userClient.js
+++ src/models/userClient.js
@@ -1,7 +1,7 @@
 const { DataTypes } = require('sequelize')
 
 module.exports = sequelize => {
-  sequelize.define('userClient', {
+  sequelize.define('client', {
     mail: {
       type: DataTypes.STRING,
       allowNull: false

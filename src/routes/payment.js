55211d30323820b4b4bb4fd177fd3f2e870ea248
Guido Ortiz <guidoortiz93@hotmail.com>
2022-04-05 14:29:40 +0200

    stripe payment

--- src/routes/payment.js
+++ src/routes/payment.js
@@ -1,7 +1,27 @@
-// const axios = require('axios')
-// const BASE = 'https://tequila-api.kiwi.com'
+const stripe = require("stripe")(process.env.STRIPE_SECRET_TEST)
 
-
-module.exports = (req, res) => {
-    res.send('todo ok')
+module.exports = async (req, res) => {
+    // let { amount, id } = req.body
+    let { amount } = req.body
+    // const amountP = parseInt(amount)
+	try {
+		const payment = await stripe.paymentIntents.create({
+			amount: amount,
+			currency: "USD",
+			description: "deviaje.com",
+			payment_method: id,
+			confirm: true
+		})
+		console.log("Payment", payment)
+		res.json({
+			message: "Payment successful",
+			success: true
+		})
+	} catch (error) {
+		console.log("Error", error)
+		res.json({
+			message: "Payment failed",
+			success: false
+		})
+	}
 }

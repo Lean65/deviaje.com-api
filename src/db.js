cc4c027f219b92be6498f7097503558ced9ab6e0
Lean65 <leandromelerio@gmail.com>
2022-04-06 16:28:09 -0300

    remove relations

--- src/db.js
+++ src/db.js
@@ -52,7 +52,7 @@ sequelize.models = Object.fromEntries(capsEntries)
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Client, Admin, Business } = sequelize.models
+// const { Client, Admin, Business } = sequelize.models
 // console.log(sequelize.models)
 
 // Aca vendrian las relaciones

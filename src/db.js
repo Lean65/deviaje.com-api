ba092b77ee3997093385650aaa5e7cb74b147571
Lean65 <leandromelerio@gmail.com>
2022-04-06 16:33:02 -0300

    admin modif

--- src/db.js
+++ src/db.js
@@ -52,11 +52,17 @@ sequelize.models = Object.fromEntries(capsEntries)
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-// const { Client, Admin, Business } = sequelize.models
+const { Client, Admin, Business } = sequelize.models
 // console.log(sequelize.models)
-
+Admin.findOrCreate({
+  mail: 'leandromelerio@gmail.com',
+  password: '1234',
+  userName: 'lean65'
+})
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
+console.log(Admin.findAll())
+console.log('estoy aca')
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');

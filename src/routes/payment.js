802c6f9c378108d70f55da9ad82094f68d0f7e7a
Lean65 <leandromelerio@gmail.com>
2022-04-08 15:20:56 -0300

    remove extra import paument

--- src/routes/payment.js
+++ src/routes/payment.js
@@ -2,8 +2,6 @@ const express = require('express')
 const router = express.Router()
 const { payment } = require('../controllers/payment')
 
-const { payment } = require('../controllers/payment')
-
 router.post('/', payment)
 
 module.exports = router

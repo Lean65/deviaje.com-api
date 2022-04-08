0990f2d15bda2ed77552fee27fd6684776e441cb
Lean65 <leandromelerio@gmail.com>
2022-04-08 15:15:27 -0300

    del

--- src/routes/updatepersonalinfo.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express')
-const router = Router()
-
-const { updatepersonalinfo } = require('../controllers/updatepersonalinfo')
-
-router.post('/', updatepersonalinfo)
-
-module.exports = router

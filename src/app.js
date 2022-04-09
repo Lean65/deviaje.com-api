d7e73f3854f1d9d4d6fa4d46a315286503b70a75
Leandro Melerio <leandromelerio@gmail.com>
2022-04-08 21:05:50 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ server.use(morgan('dev'))
 server.use(express.json({ extended: true }))
 server.use(express.urlencoded({ extended: true }))
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://travelco.vercel.app')
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
   res.header('Access-Control-Allow-Credentials', 'true')
   res.header(
     'Access-Control-Allow-Headers',

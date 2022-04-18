01d943611fdd188b281fd1844c4bdbfc3d6987fd
Lean65 <leandromelerio@gmail.com>
2022-04-18 18:58:45 -0300

    asdas

--- src/controllers/searchPassengers.js
+++ src/controllers/searchPassengers.js
@@ -12,16 +12,18 @@ module.exports = {
       .then(r=>{
         return res.send(r)
       })
-    } 
-    Passenger.findAll()
-      .then(r =>{
-        return res.send(
-          r.filter(u => u.name.toLowerCase().includes(name.toLowerCase()))
-        )
-      })
-      .catch(() => {
-        return handleHttpError(res, 'ERROR_PASSENGER_DO_NOT_FOUND')
-        //res.send('Paso algo malo con la info personal...')
-      })
+    }
+    else {
+      Passenger.findAll()
+        .then(r =>{
+          r = r.filter(u => u.name.toLowerCase().includes(name.toLowerCase()))
+          if(r.length < 1) return res.send('No hay coincidencias')
+          return res.send(r)
+        })
+        .catch(() => {
+          return handleHttpError(res, 'ERROR_PASSENGER_DO_NOT_FOUND')
+          //res.send('Paso algo malo con la info personal...')
+        })
+    }
   }
 }

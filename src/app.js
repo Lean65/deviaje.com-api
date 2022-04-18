8d94239b91033af23d629fda8fbe0ab16f51cc87
Lean65 <leandromelerio@gmail.com>
2022-04-18 10:13:50 -0300

    added * to app.js

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ server.use(morgan('dev'))
 server.use(express.json({ extended: true }))
 server.use(express.urlencoded({ extended: true }))
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
+  res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Credentials', 'true')
   res.header(
     'Access-Control-Allow-Headers',
